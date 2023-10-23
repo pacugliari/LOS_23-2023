1505bd09e2c1e989fc248fb6c24eed5ccfd5e37a
pacugliari <pacugliari@hotmail.com>
2023-10-23 10:25:39 -0300

    Proyecto base

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { firestore, storage } from 'src/main';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,9 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() {
+    console.log(firestore)
+    console.log(storage)
+  }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,24 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { environment, firebaseConfig } from './environments/environment';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 if (environment.production) {
   enableProdMode();
 }
+
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
+export const storage = getStorage(app);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
